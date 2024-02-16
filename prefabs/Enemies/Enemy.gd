25047f42878886e0dda240e18b94834eae3da212
blockycow <blockycow@gmail.com>
2024-02-16 14:12:54 +0100

    Added a basic enemy that moves towards the player

---
+++ prefabs/Enemies/Enemy.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var movement_speed = 20.0
+@export var hp = 10
+@export var knockback_recovery = 3.5
+@export var experience = 1
+@export var enemy_damage = 1
+var knockback = Vector2.ZERO
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite = $Sprite2D
+@onready var hitBox = $HitBox
+
+signal remove_from_array(object)
+
+func _ready():
+	hitBox.damage = enemy_damage
+	
+func _physics_process(_delta):
+	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction*movement_speed
+	velocity += knockback
+	move_and_slide()
+	
+	if direction.x > 0.1:
+		sprite.flip_h = true
+	elif direction.x < -0.1:
+		sprite.flip_h = false
+		
+
